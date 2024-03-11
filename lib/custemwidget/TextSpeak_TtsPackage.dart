a3fce427c35acaed915fb02ec546b77c13e5b34c
MuhammadIshaq70 <muhammadishaq2921217@gmail.com>
2024-03-11 17:34:03 +0500

    1.1

---
+++ lib/custemwidget/TextSpeak_TtsPackage.dart
@@ -0,0 +1,19 @@
+
+// ///////////////////////////////////class _ProfileScreenState extends State<ProfileScreen> { ///////////////////////////////////
+//    final FlutterTts flutterTts = FlutterTts();
+//   Future<dynamic> speak(String text) async {
+//     await flutterTts.setLanguage('en-Us');
+//     await flutterTts.setPitch(1); // 0.5 to 1.5
+//     await flutterTts.speak(text);
+//   }
+// //////////////////////////////////////////////////////////Package///////////////////////////////////
+// flutter_tts
+
+
+// ///////////////////////////////////////////////body///////////////////////////////////////////////////////
+//  InkWell(
+//                                 onTap: () {
+//                                   speak(controller.text);
+//                                 },
+//                                 child: const Icon(Icons.volume_up_outlined)),
+
